3bfbc60cd550abf8e4194848a8fe39b11a904223
allan <allen.nassif@campus.technion.ac.il>
2022-11-04 16:12:24 +0200

    fixed the syntax of print_number.c

--- print_number.c
+++ print_number.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#define SIZE_OF_COURSE_NUMBER 6
 
 int main() {
-	int course_number[6] = {4, 2, 1, 4, 3, 2};
-	for(int i = 5; i >= 0; i--) {
+	int course_number[SIZE_OF_COURSE_NUMBER] = {4, 2, 1, 4, 3, 2};
+	for(int i = SIZE_OF_COURSE_NUMBER-1 ; i >= 0; i--) {
 		printf("%d", course_number[i]);
 	}
 	printf("\n");
